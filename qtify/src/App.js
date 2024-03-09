42e24b23b77f04a21a33d783bf5673b96e00d896
Srijalsahu <srijalsahu007@gmail.com>
2024-03-09 19:40:56 +0530

    f

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,33 @@
-import Hero from "./components/Hero/Hero";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import {fetchTopAlbums, fetchNewAlbums, fetchSongs} from ".api/api"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [searchData, useSearchData] = useState();
+  const [data, setData] = useState({});
+  const generateData = (key, source) =>{
+    source().then((data) => {
+      setData((prevData) =>{
+        return {...prevData, [key]: data};
+      })
+    })
+  }
+
+  useEffect(()=>{
+    generateData("topAlbums",fetchTopAlbums);
+    generateData("newAlbums",fetchNewAlbums);
+    generateData("songs",fetchSongs);
+  },[]);
+  const {topAlbums =[], newAlbums=[], songs=[]}=data;
+
   return (
     <>
-    <Navbar/>
-    <Hero/>
+    <div>
+      <Navbar/>
+      <Outlet context={{ data: {topAlbums, newAlbums, songs}}}/>
+    </div>
+    
     </>
   );
 }
